c578a5ad0c468d23eb9dda144f75b1949670c88f
Vikash Kumar <21je1044@iitism.ac.in>
2023-07-08 14:48:55 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const client = require("@mailchimp/mailchimp_marketing");
+require("dotenv").config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+client.setConfig({
+    apiKey: process.env.API_KEY,
+    server: process.env.API_SERVER,
+});
+
+app.post("/", (req, res) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    const run = async () => {
+        const response = await client.lists.batchListMembers(process.env.LIST_ID, {
+          members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+          ],
+        });
+        if(response.errors.length === 0){
+            res.sendFile(__dirname + "/success.html");
+        }else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+      };
+      
+    run();
+});
+
+app.post("/failure", (req, res) => {
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running on port 3000.");
+});
